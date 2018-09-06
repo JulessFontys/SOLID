d2b4cbc825cc8bcf98525f8b65ce76a03f58555e
JulessFontys <43028233+JulessFontys@users.noreply.github.com>
2018-09-06 11:13:11 +0200

    Initial commit

---
+++ Assets/Script/Movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    private InputBehaviour _input;
+
+	// Use this for initialization
+	void Start ()
+    {
+        _input = GetComponent<InputBehaviour>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (_input.Tap())
+        {
+            Debug.Log("Move!");
+        }
+
+        if (_input.DoubleTap())
+        {
+            Debug.Log("Dash!");
+        }
+
+
+
+/*Dash code here*/
+
+    }
+}
